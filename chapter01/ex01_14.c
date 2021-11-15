f959aecd43f453be3f973a4bc1c398ce5a0ab496
Darío Álvarez <vidaalhe90@gmail.com>
2021-11-15 04:02:11 -0600

    Add exercise 1.14 solution

---
+++ chapter01/ex01_14.c
@@ -0,0 +1,41 @@
+
+/* Exercise 1-14.
+ * Write a program to print a histogram of the frequencies
+ * of different characters in its input. */
+
+#include <stdio.h>
+
+#define NUM_PRINTABLE_CHARS '~' - ' ' + 1
+
+int main(void)
+{
+    int i, j, c;
+    int frequencies[NUM_PRINTABLE_CHARS];
+
+    // Initialize frequencies array.
+    for (i = 0; i < NUM_PRINTABLE_CHARS; i++) { frequencies[i] = 0; }
+
+    // Compute frequencies for each printable character entered.
+    while ((c = getchar()) != EOF)
+    {
+        if (c >= ' ' && c <= '~') { frequencies[c - ' '] += 1; }
+    }
+
+    // Print histogram.
+    // Print histograms of lenghts.
+    printf("\n==== Histogram of frequencies (c) of characters ====\n");
+    printf("-------------------------------------------------------\n");
+    printf("  c  |                      count                      \n");
+    printf("-------------------------------------------------------\n");
+    for (i = 0; i < NUM_PRINTABLE_CHARS; i++)
+    {
+        if (frequencies[i] != 0)
+        {
+            printf("  %c  |", (i + ' '));
+            for (j = 1; j <= frequencies[i]; j++) { printf("*"); }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
