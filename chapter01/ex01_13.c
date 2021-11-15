3af8f436964589ca069e7001629096d8402a9795
Darío Álvarez <vidaalhe90@gmail.com>
2021-11-15 03:32:24 -0600

    Fix som comments

--- chapter01/ex01_13.c
+++ chapter01/ex01_13.c
@@ -1,8 +1,8 @@
 
-/**Exercise 1-13.
+/* Exercise 1-13.
  * Write a program to print a histogram of the lenghts of words in
  * its input. It is easy to draw the histogram with the bars horizontal;
- * a vertical orientation is more challenging. **/
+ * a vertical orientation is more challenging. */
 
 #include <stdio.h>
 
