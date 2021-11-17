7e2f6b1c73bf291b5a24bd497bbfdb20acdd6b3e
Darío Álvarez <vidaalhe90@gmail.com>
2021-11-17 06:32:43 -0600

    Modify code style and fix some comments

--- chapter01/ex01_04.c
+++ chapter01/ex01_04.c
@@ -5,20 +5,18 @@
 
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
     float celsius, fahr;
     int lower, upper, step;
 
-    // Set initial values for lower, upper, and step.
+    /* Set initial values for lower, upper, and step. */
     lower = 0;
     upper = 300;
     step = 20;
 
-    // Compute and print fahr values for celsius = 0, 20, ..., 300.
+    /* Compute and print fahr values for celsius = 0, 20, ..., 300. */
     celsius = lower;
-    while (celsius <= upper)
-    {
+    while (celsius <= upper) {
         fahr = (9.0 / 5.0) * celsius + 32.0;
         printf("%3.0f %6.1f\n", celsius, fahr);
         celsius = celsius + step;
